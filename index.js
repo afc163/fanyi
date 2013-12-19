c41c2ba22337a07e297093b63ea60cec3dca5dc3
afc163 <afc163@gmail.com>
2013-12-19 22:09:48 +0800

    optimize code

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var request = require('request');
 var parser = require('xml2json');
 var SOURCE = require('./lib/source.json');
 var print = require('./lib/print');
+var Entities = require('html-entities').AllHtmlEntities;
+entities = new Entities();
 
 module.exports = function(word) {
   request.get(SOURCE['iciba'] + encodeURIComponent(word), function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var data = JSON.parse(parser.toJson(body)).dict;
+      var data = JSON.parse(entities.decode(parser.toJson(body))).dict;
       print.iciba(data);
     }
   });
   request.get(SOURCE['youdao'] + encodeURIComponent(word), function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
+      var data = JSON.parse(entities.decode(body));
       print.youdao(data);
     }
   });
