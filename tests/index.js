7456fa8bcbf0e19a3ed00c76042db7d41f9dcbac
afc163 <afc163@gmail.com>
2015-05-18 12:03:52 +0800

    fix test case

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,11 @@
 var coffee = require('coffee');
+var version = require('../package').version;
+console.log(version);
 
 describe('fanyi', function() {
   it('should show help info', function(done) {
     coffee.spawn('fanyi')
-      .expect('stdout', 'fanyi ~ 0.2.5\nTranslate tools in command line\n  $ fanyi word\n  $ fanyi world peace\n  $ fanyi chinglish\n')
+      .expect('stdout', 'fanyi ~ ' + version + '\nTranslate tools in command line\n  $ fanyi word\n  $ fanyi world peace\n  $ fanyi chinglish\n')
       .expect('code', 0)
       .end(done);
   });
