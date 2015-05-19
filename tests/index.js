7ea8b151ea1d83651d06cc73e12affb44f0c144e
afc163 <afc163@gmail.com>
2015-05-19 17:12:40 +0800

    use fork to test bin

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,18 @@
 var coffee = require('coffee');
+var path = require('path');
 var version = require('../package').version;
-console.log(version);
 
 describe('fanyi', function() {
   it('should show help info', function(done) {
-    coffee.spawn('fanyi')
+    coffee.fork(path.join(__dirname, '../bin/fanyi'))
       .expect('stdout', 'fanyi ~ ' + version + '\nTranslate tools in command line\n  $ fanyi word\n  $ fanyi world peace\n  $ fanyi chinglish\n')
       .expect('code', 0)
       .end(done);
   });
 
   it('should translate word', function(done) {
-    coffee.spawn('fanyi', ['word'])
+    coffee.fork(path.join(__dirname, '../bin/fanyi'), ['word'])
+      .expect('stdout', /word  \[ wɜːd \]  ~  fanyi\.youdao\.com/gi)
       .expect('code', 0)
       .end(done);
   });
