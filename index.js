bfeef2af2953afb799e34ed234925888572196e5
afc163 <afc163@gmail.com>
2015-05-20 12:16:44 +0800

    user xml2js replace xml2json, improve install experience

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var request = require('request');
-var parser = require('xml2json');
 var SOURCE = require('./lib/source');
 var print = require('./lib/print');
 var spawn = require('child_process').spawn;
 var Entities = require('html-entities').AllHtmlEntities;
 entities = new Entities();
+var parseString = require('xml2js').parseString;
 
 module.exports = function(word) {
 
   // avoid no say command
   process.on('uncaughtException', function(err) {
     if (err.toString().indexOf('spawn ENOENT') < 0) {
-      console.log(err);
+      console.log(err.stack);
     }
   });
 
@@ -23,11 +23,9 @@ module.exports = function(word) {
   // iciba
   request.get(SOURCE.iciba + word, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      // escape " -> '
-      body = body.replace(/&quot;/g, '&#39;');
-      // iciba need twice decode ...
-      var data = JSON.parse(entities.decode(entities.decode(parser.toJson(body)))).dict;
-      print.iciba(data);
+      parseString(body, function (err, result) {
+        print.iciba(result.dict);
+      });
     }
   });
 
