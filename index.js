079a97f3d5997fb6779b4e2cfd8c8e3ee3d22cbb
foosamz <beastfoodie@163.com>
2020-11-26 14:53:13 +0800

    feat: The user can disable or enable the translation engine and save the configuration to ~/.fanyirc under user's home directory

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ const ora = require('ora');
 
 module.exports = function (word, options, callback) {
   console.log('');
+  const { say, iciba, youdao, dictionaryapi } = options;
+  const requestCounts = [iciba, youdao, dictionaryapi].filter(isTrueOrUndefined).length;
   const spinner = ora().start();
+
   // say it
   try {
-    if (!process.env.CI) {
+    if (!process.env.CI && isTrueOrUndefined(say)) {
       require('say').speak(word, isChinese(word) ? 'Ting-Ting' : null);
     }
   } catch (e) {
@@ -22,7 +25,7 @@ module.exports = function (word, options, callback) {
   let count = 0;
   const callbackAll = () => {
     count += 1;
-    if (count >= 3) {
+    if (count >= requestCounts) {
       spinner.stop();
       callback && callback();
     }
@@ -31,47 +34,54 @@ module.exports = function (word, options, callback) {
   word = encodeURIComponent(word);
 
   // iciba
-  request.get(SOURCE.iciba.replace('${word}', word), function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      parseString(body, function (err, result) {
-        if (err) {
-          return;
-        }
-        print.iciba(result.dict, options);
-      });
-    }
-    callbackAll();
-  });
+  isTrueOrUndefined(iciba) &&
+    request.get(SOURCE.iciba.replace('${word}', word), function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        parseString(body, function (err, result) {
+          if (err) {
+            return;
+          }
+          print.iciba(result.dict, options);
+        });
+      }
+      callbackAll();
+    });
 
   // youdao
-  request.get(SOURCE.youdao.replace('${word}', word), function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      try {
-        const data = JSON.parse(entities.decode(body));
-        print.youdao(data, options);
-      } catch (e) {
-        // 来自您key的翻译API请求异常频繁，为保护其他用户的正常访问，只能暂时禁止您目前key的访问
+  isTrueOrUndefined(youdao) &&
+    request.get(SOURCE.youdao.replace('${word}', word), function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        try {
+          const data = JSON.parse(entities.decode(body));
+          print.youdao(data, options);
+        } catch (e) {
+          // 来自您key的翻译API请求异常频繁，为保护其他用户的正常访问，只能暂时禁止您目前key的访问
+        }
       }
-    }
-    callbackAll();
-  });
+      callbackAll();
+    });
 
   // dictionaryapi
-  request.get(SOURCE.dictionaryapi.replace('${word}', word), { timeout: 6000 }, function (
-    error,
-    response,
-    body,
-  ) {
-    if (error) {
-      return callbackAll();
-    }
-    if (response.statusCode == 200) {
-      parseString(body, function (err, result) {
-        if (!err) {
-          print.dictionaryapi(result.entry_list.entry, word, options);
+  isTrueOrUndefined(dictionaryapi) &&
+    request.get(
+      SOURCE.dictionaryapi.replace('${word}', word),
+      { timeout: 6000 },
+      function (error, response, body) {
+        if (error) {
+          return callbackAll();
         }
-      });
-    }
-    callbackAll();
-  });
+        if (response.statusCode == 200) {
+          parseString(body, function (err, result) {
+            if (!err) {
+              print.dictionaryapi(result.entry_list.entry, word, options);
+            }
+          });
+        }
+        callbackAll();
+      },
+    );
 };
+
+function isTrueOrUndefined(val) {
+  return val === true || val === undefined;
+}
